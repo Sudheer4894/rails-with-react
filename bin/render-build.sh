863c30e6b4c635baf81912e67534558b6ad156f4
Sudheer <kumarsudheer4894@gmail.com>
2023-10-16 09:36:53 +0530

    configuration 7

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 bundle install
 yarn install
 bundle exec rake assets:clobber
-bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rake assets:precompile
 bundle exec rake db:migrate
 bundle exec rake db:seed
